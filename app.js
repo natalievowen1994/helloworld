a447d4691ecab050b39a30f95c9285ad4401093f
Owen <natalie.owen@cgi.com>
2016-07-17 18:24:49 +0100

    commit 2

--- app.js
+++ app.js
@@ -1,57 +1,15 @@
-var http      = require('http');
-var mongoose  = require('mongoose');
-var express   = require('express');
-
-var app    = express();
-var db;
-
-var config = {
-      "USER"    : "ec2-user",    
-      "PASS"    : "",
-      "HOST"    : "ec2-52-34-172-169.us-west-2.compute.amazonaws.com",  
-      "PORT"    : "27017", 
-      "DATABASE" : "my_example"
-    };
-
-var dbPath  = "mongodb://"+config.USER + ":"+
-    config.PASS + "@"+
-    config.HOST + ":"+
-    config.PORT + "/"+
-    config.DATABASE;
-var standardGreeting = 'Hello World!';
-
-var greetingSchema = mongoose.Schema({
-  sentence: String
-}); 
-var Greeting= mongoose.model('Greeting', greetingSchema);
-
-db = mongoose.connect(dbPath);
-
-mongoose.connection.once('open', function() {
-  var greeting;
-  Greeting.find( function(err, greetings){
-   if( !greetings ){     
-      greeting = new Greeting({ sentence: standardGreeting }); 
-      greeting.save();
-    } 
-  }); 
+var MongoClient = require('mongodb').MongoClient;
+ 
+var myCollection;
+var db = MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if(err)
+        throw err;
+    console.log("connected to the mongoDB !");
+    myCollection = db.collection('test_collection');
+    
+    myCollection.insert({name: "doduck", description: "learn more than everyone"}, function(err, result) {
+    if(err)
+        throw err;
+ 
+    console.log("entry saved");
 });
-
-app.get('/', function(req, res){
-  Greeting.findOne(function (err, greeting) {
-    res.send(greeting.sentence);
-  });
-});
-
-app.use(function(err, req, res, next){
-  if (req.xhr) {
-    res.send(500, 'Something went wrong!');
-  }
-  else {
-    next(err);
-  }
-});
-
-console.log('starting the Express (NodeJS) Web server');
-app.listen(8080);
-console.log('Webserver is listening on port 8080');
